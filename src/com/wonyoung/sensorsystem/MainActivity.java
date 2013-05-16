f592b7d70f506670e9a749d322e08631a1b455a3
wonyoung <wonyoungjjang@gmail.com>
2013-05-16 13:09:22 +0900

    Passed 'shows sensor name' test

--- src/com/wonyoung/sensorsystem/MainActivity.java
+++ src/com/wonyoung/sensorsystem/MainActivity.java
@@ -1,15 +1,38 @@
 package com.wonyoung.sensorsystem;
 
+import java.util.ArrayList;
+
 import android.os.Bundle;
 import android.app.Activity;
 import android.view.Menu;
+import android.view.View;
+import android.view.View.OnClickListener;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.ListView;
 
 public class MainActivity extends Activity {
+
+	private ArrayList<String> sensors = new ArrayList<String>();
 
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		ListView sensorListView = (ListView) findViewById(R.id.sensors_listview);
+		final ArrayAdapter<String> arrayAdapter = new ArrayAdapter<String>(this, R.layout.sensors_item, sensors);
+		sensorListView.setAdapter(arrayAdapter);
+		
+		Button addButton = (Button) findViewById(R.id.add_button);
+		addButton.setOnClickListener(new OnClickListener() {
+			
+			@Override
+			public void onClick(View v) {
+				sensors.add("Sensor 1");
+				arrayAdapter.notifyDataSetChanged();
+			}
+		});
 	}
 
 	@Override
