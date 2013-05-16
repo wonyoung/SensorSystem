f43cc80aa22363174b8017341e387e07d10e67de
wonyoung <wonyoungjjang@gmail.com>
2013-05-16 15:13:03 +0900

    Refactoring test - made setUp()

--- test/src/MainActivityTest.java
+++ test/src/MainActivityTest.java
@@ -2,6 +2,7 @@ import static org.hamcrest.CoreMatchers.equalTo;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertThat;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.robolectric.Robolectric;
@@ -15,14 +16,19 @@ import com.wonyoung.sensorsystem.MainActivity;
 @RunWith(RobolectricTestRunner.class)
 public class MainActivityTest {
 
-	@Test
-	public void showsSensorNameWhenSensorAdded() throws Exception {
+	private SensorSystemDriver driver;
+
+	@Before
+	public void setUp() throws Exception {
 		Activity activity = Robolectric.buildActivity(MainActivity.class)
 				.create()
 				.get();
 		
-		SensorSystemDriver driver = new SensorSystemDriver(activity);
-				
+		driver = new SensorSystemDriver(activity);
+	}
+
+	@Test
+	public void showsSensorNameWhenSensorAdded() throws Exception {
 		assertEquals(0, driver.sensors());
 		driver.addSensor();
 		assertEquals(1, driver.sensors());
